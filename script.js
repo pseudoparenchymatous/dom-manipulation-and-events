6f63d1b3585f55f8b5e2df9c86b1cad59ddd4a5b
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-21 20:06:31 +0800

    add pink div element

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ container.appendChild(para);
 const h3 = document.createElement('h3');
 h3.textContent = 'I\'m a blue h3!';
 container.appendChild(h3);
+
+const div = document.createElement('div');
+div.classList.toggle('pink-div');
+div.style.cssText = 'border: solid black 2px; background-color: pink;';

32aca8c2c17460cc45645dd035019992fb6e8824
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-21 19:57:24 +0800

    add 'h3' element

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const container = document.querySelector('#container');
 const para = document.createElement('p');
 para.textContent = 'Hey I\'m red!';
 container.appendChild(para);
+
+const h3 = document.createElement('h3');
+h3.textContent = 'I\'m a blue h3!';
+container.appendChild(h3);

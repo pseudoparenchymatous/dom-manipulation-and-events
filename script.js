db27288c3ec79dc862b8624b1507835cef2f0558
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-21 20:07:13 +0800

    add h1 element inside pink div

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ container.appendChild(h3);
 const div = document.createElement('div');
 div.classList.toggle('pink-div');
 div.style.cssText = 'border: solid black 2px; background-color: pink;';
+
+const h1 = document.createElement('h1');
+h1.textContent = 'I\'m in a div';
+div.appendChild(h1);
